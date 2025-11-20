ac0cb87aaf2661889a2eddf963651abfae6bf14f
yesesvee04 <spasupul7@gitam.in>
2025-11-20 20:27:58 +0530

    Redefined classes and created hashmap for floorCategoryRooms and roomsAvailable. Added a room display funtion.

--- HotelManagement.java
+++ HotelManagement.java
@@ -1,81 +1,82 @@
-import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.io.File;
-import java.io.IOException;
-class CustomerDetails{
-    private String name;
-    private long phoneNumber;
-    private int age;
-    private int days;
-    private int category;
-    private int roomNo;
+import java.util.*;
+import java.io.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
-    public CustomerDetails(String name,long phoneNumber,int age,int days,int category,int roomNo){
+class CustomerDetails{
+    //Created a class of customer details(name,phone number,age,room no,leaving date) which can be used to 
+    // view the customer details. 
+    String name;
+    long phoneNumber;
+    int age;
+    int roomNo;
+    String leavingDate;
+    
+    public CustomerDetails(String name,long phoneNumber,int age,int roomNo,String leavingDate){
         this.name=name;
         this.phoneNumber=phoneNumber;
         this.age=age;
-        this.days=days;
-        this.category=category;
         this.roomNo=roomNo;
-    }
-
-    public String getName(){
-        return name;
-    }
-    public long getPhoneNumber(){
-        return phoneNumber;
-    }
-    public int getAge(){
-        return age;
-    }
-    public int getDays(){
-        return days;
-    }
-    public int getCategory(){
-        return category;
-    }
-    public int getRoomNo(){
-        return roomNo;
+        this.leavingDate=leavingDate;
     }
 }
-public class HotelManagement{
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        Map<Integer, List<Integer>> categories = new HashMap<>();
-        categories.put(1, Arrays.asList(101, 102, 201, 202, 301, 302, 401, 402, 501, 502));
-        categories.put(2, Arrays.asList(103, 104, 203, 204, 303, 304, 403, 404, 503, 504));
-        categories.put(3, Arrays.asList(105, 106, 205, 206, 305, 306, 405, 406, 505, 506));
-        categories.put(4, Arrays.asList(107, 108, 207, 208, 307, 308, 407, 408, 507, 508));
 
-        Map<Integer,Boolean> roomAvailability=Map.ofEntries(
-            Map.entry(101,false),Map.entry(102,false),Map.entry(103,false),Map.entry(104,false),
-            Map.entry(105,false),Map.entry(106,false),Map.entry(107,false),Map.entry(108,false), 
-            Map.entry(201,false),Map.entry(202,false),Map.entry(203,false),Map.entry(204,false),
-            Map.entry(205,false),Map.entry(206,false),Map.entry(207,false),Map.entry(208,false),
-            Map.entry(301,false),Map.entry(302,false),Map.entry(303,false),Map.entry(304,false),
-            Map.entry(305,false),Map.entry(306,false),Map.entry(307,false),Map.entry(308,false), 
-            Map.entry(401,false),Map.entry(402,false),Map.entry(403,false),Map.entry(404,false),
-            Map.entry(405,false),Map.entry(406,false),Map.entry(407,false),Map.entry(408,false),
-            Map.entry(501,false),Map.entry(502,false),Map.entry(503,false),Map.entry(504,false),
-            Map.entry(505,false),Map.entry(506,false),Map.entry(507,false),Map.entry(508,false)
-            );
-        try{
-            File myFile=new File("/Users/yesesvee/Desktop/Hotel_Management/Customer_Details.txt");
-            if(myFile.createNewFile()){
-                System.out.println("New file created:"+myFile.getName());
-            }
-            else{
-                System.out.println("File already exists");
+class RoomAvailability{
+    //Created a room availability class so that we can check how many rooms are currently available and 
+    // leaving date is stored in local date time format to check and replace the room availability after 
+    // customer leaves.
+    int roomNo;
+    boolean isAvailable;
+    LocalDateTime leavingDate;
+
+    public RoomAvailability(int roomNo){
+        this.roomNo=roomNo;
+        this.isAvailable=true;
+        this.leavingDate=null;
+    }
+
+}
+public class HotelManagement{
+    public static final Map<Integer,Map<Integer,List<Integer>>> floorCategoryRooms=new HashMap<>();
+    //floorCategoryRooms is a hashmap where 1st map contains key as floor(1,2,3,4,5) and its values are 
+    // map containing key as categories(1-Non AC,2-AC,3-Beach view Non-AC,4-Beach view AC) and its 
+    // values are room nos on each floor which match the category(1-1(Non-AC)-[101,102])
+    public static final Map<Integer,RoomAvailability> roomAvailabilityStatus=new HashMap<>();
+    //roomAvailabilityStatus is a hashmap where integer is room no(101) and RoomAvailabile is a newly created object 
+    // of room no(101) which has isAvailable-true and leavingDate=null.
+    public static final List<CustomerDetails> customer=new ArrayList<>();
+    //It stores the list of customers details.
+    
+    
+    public static void main(String[] args) throws IOException{
+        initializeRooms();
+        displayFloorCategoryRooms();
+    }
+    public static void initializeRooms(){
+        for(int floor=1;floor<=5;floor++){
+            Map<Integer,List<Integer>> categoryRooms=new HashMap<>();
+            categoryRooms.put(1,Arrays.asList(floor*100+1,floor*100+2));
+            categoryRooms.put(2,Arrays.asList(floor*100+3,floor*100+4));
+            categoryRooms.put(3,Arrays.asList(floor*100+5,floor*100+6));
+            categoryRooms.put(4,Arrays.asList(floor*100+7,floor*100+8));
+            floorCategoryRooms.put(floor,categoryRooms);
+
+            for(List<Integer> rooms:categoryRooms.values()){
+                for(int room:rooms){
+                    roomAvailabilityStatus.put(room,new RoomAvailability(room));
+                }
             }
         }
-        catch(IOException e){
-            System.out.println("An error occured");
-            e.printStackTrace();
+    }
+    public static void displayFloorCategoryRooms(){
+        for(Map.Entry<Integer,Map<Integer,List<Integer>>> floorEntry:floorCategoryRooms.entrySet()){
+            int floor=floorEntry.getKey();
+            Map<Integer,List<Integer>> categories=floorEntry.getValue();
+            for(Map.Entry<Integer,List<Integer>> categoryEntry:categories.entrySet()){
+                int category=categoryEntry.getKey();
+                List<Integer> rooms=categoryEntry.getValue();
+                System.out.println("Floor:"+floor+" Category:"+category+" Rooms:"+rooms);
+            }
         }
-        sc.close();
     }
 }
